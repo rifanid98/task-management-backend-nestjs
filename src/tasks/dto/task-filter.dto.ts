e8869fbd9c052a1f19cf03390e216a5e00572a24
rifanid <adninsijawa.office@gmail.com>
2021-06-10 16:42:05 +0700

    validation pipe - validationg filtering and searching

--- src/tasks/dto/task-filter.dto.ts
+++ src/tasks/dto/task-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsIn, IsOptional, IsString } from 'class-validator';
 import { Status } from '../tasks.model';
 
 export class TaskFilterDto {
+  @IsOptional()
+  @IsIn([...Object.keys(Status)])
   status?: Status;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
