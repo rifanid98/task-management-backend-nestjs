ab61f22a04d8da0038d96eaed72eb00434d3013d
rifanid98 <adninsijawa.office@gmail.com>
2021-06-14 11:18:44 +0700

    fixing: update typeorm config

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,20 +3,23 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeormConfig = async (
   configService: ConfigService,
-): Promise<TypeOrmModuleOptions> => ({
-  ssl: configService.get('NODE_ENV') === 'production',
-  extra: {
-    ssl:
-      configService.get('NODE_ENV') === 'production'
-        ? { rejectUnathorized: false }
-        : null,
-  },
-  type: 'postgres',
-  host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT'),
-  username: configService.get('DB_USER'),
-  password: configService.get('DB_PASS'),
-  database: configService.get('DB_NAME'),
-  autoLoadEntities: true,
-  synchronize: true,
-});
+): Promise<TypeOrmModuleOptions> => {
+  const isProduction = configService.get('NODE_ENV') === 'production';
+
+  const typeOrmModuleOptions: TypeOrmModuleOptions = {
+    ssl: isProduction,
+    extra: {
+      ssl: isProduction ? { rejectUnathorized: false } : null,
+    },
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USER'),
+    password: configService.get('DB_PASS'),
+    database: configService.get('DB_NAME'),
+    autoLoadEntities: true,
+    synchronize: true,
+  };
+
+  return typeOrmModuleOptions;
+};
